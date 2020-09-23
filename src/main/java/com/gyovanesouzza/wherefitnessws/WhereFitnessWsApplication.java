f19dae8b10f41c462a2df64149548d574f6a4edc
gyovanesouzza <gyovanesouzza>
2020-09-23 12:11:24 -0300

    Init project

---
+++ src/main/java/com/gyovanesouzza/wherefitnessws/WhereFitnessWsApplication.java
@@ -0,0 +1,13 @@
+package com.gyovanesouzza.wherefitnessws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereFitnessWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereFitnessWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyovanesouzza/wherefitnessws/WhereFitnessWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyovanesouzza.wherefitnessws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereFitnessWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
